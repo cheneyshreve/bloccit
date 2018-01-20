6b4bba77258bdbb6ba695b740972184ceab84323
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-01-19 16:56:41 -0800

    checkpoint_16_auth completed: added guest,admin and user roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119210155) do
-
-  # create_table "advertisements", force: :cascade do |t|
-  #   t.string "title"
-  #   t.text "copy"
-  #   t.integer "price"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
-  # create_table "answers", force: :cascade do |t|
-  #   t.text "body"
-  #   t.integer "question_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["question_id"], name: "index_answers_on_question_id"
-  # end
+ActiveRecord::Schema.define(version: 20180119234343) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -47,24 +31,6 @@ ActiveRecord::Schema.define(version: 20180119210155) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  # create_table "questions", force: :cascade do |t|
-  #   t.string "title"
-  #   t.text "body"
-  #   t.boolean "resolved"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
-  # create_table "sponsored_posts", force: :cascade do |t|
-  #   t.string "title"
-  #   t.text "body"
-  #   t.integer "price"
-  #   t.integer "topic_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["topic_id"], name: "index_sponsored_posts_on_topic_id"
-  # end
-
   create_table "topics", force: :cascade do |t|
     t.string "name"
     t.boolean "public", default: true
@@ -79,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180119210155) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
