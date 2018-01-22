d0d740dfe7b5d019be07a113ffa00639e2285731
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-01-21 16:06:42 -0800

    checkpoint_18 completed: added up/down vote

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,23 @@ end
 topics = Topic.all
 
 50.times do
-   Post.create!(
-     user: users.sample,
-     topic: topics.sample,
-     title: RandomData.random_sentence,
-     body: RandomData.random_paragraph
+  Post.create!(
+     user:   users.sample,
+     topic:  topics.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
    )
 end
 posts = Post.all
+
+post = Post.create!(
+   user:   users.sample,
+   topic:  topics.sample,
+   title:  RandomData.random_sentence,
+   body:   RandomData.random_paragraph
+ )
+post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 
 100.times do
   Comment.create!(
@@ -55,3 +64,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
